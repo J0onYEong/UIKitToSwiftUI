4c820224b57beda158c88d362fb1efdc43fc5891
J0onYEong <cjy2134@naver.com>
2023-11-18 15:04:55 +0900

    [Add] CJMapkitView Annotations & specifying user location test

--- Project.swift
+++ Project.swift
@@ -3,14 +3,17 @@ import ProjectDescriptionHelpers
 
 // MARK: - Project
 
-let basePath = "Targets/UIKitToSwiftUI"
-let bundleId = "io.tuist.ProjectDescriptionHelpers"
-let packagePath = "../Packages"
+private let version: String = "0.0.1"
+private let bundleId = "io.tuist.ProjectDescriptionHelpers"
+private let bundleVersion: String = "1"
+private let iOSTargetVersion: String = "16.0"
+private let basePath = "Targets/UIKitToSwiftUI"
+private let localPackagePath = "../Packages"
 
 let project = Project(
         name: "UIKitToSwiftUI",
         packages: [
-            .local(path: "\(packagePath)/MapkitToSwiftUI")
+            .package(path: "\(localPackagePath)/MapkitToSwiftUI")
         ],
         targets: [
             Target(name: "UIKitToSwiftUI",
@@ -18,7 +21,9 @@ let project = Project(
                    product: .app,
                    bundleId: bundleId,
                    deploymentTarget: .iOS(targetVersion: "16.0", devices: .iphone),
+                   infoPlist: makeInfoPlist(),
                    sources: ["\(basePath)/Sources/**"],
+                   resources: ["\(basePath)/Resources/**"],
                    dependencies: [
                         .package(product: "MapkitToSwiftUI")
                    ]
@@ -32,5 +37,28 @@ let project = Project(
                     .target(name: "UIKitToSwiftUI")
                 ]
             )
+        ],
+        additionalFiles: [
+            "README.md"
         ]
 )
+
+private func makeInfoPlist(merging other: [String: Plist.Value] = [:]) -> InfoPlist {
+    var extendedPlist: [String: Plist.Value] = [
+        "UIApplicationSceneManifest": ["UIApplicationSupportsMultipleScenes": true],
+        "UILaunchScreen": [],
+        "UISupportedInterfaceOrientations":
+            [
+                "UIInterfaceOrientationPortrait"
+            ],
+        "CFBundleShortVersionString": "\(version)",
+        "CFBundleVersion": "\(bundleVersion)",
+        "CFBundleDisplayName": "$(APP_DISPLAY_NAME)"
+    ]
+
+    other.forEach { (key: String, value: Plist.Value) in
+        extendedPlist[key] = value
+    }
+
+    return InfoPlist.extendingDefault(with: extendedPlist)
+}
--- Targets/UIKitToSwiftUI/Sources/App.swift
+++ Targets/UIKitToSwiftUI/Sources/App.swift
@@ -2,12 +2,56 @@
 
 import SwiftUI
 import MapkitToSwiftUI
+import CoreLocation
 
 @main
 struct UIKitToSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            MapKitViewView()
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var currentLocation = CLLocation(latitude: 0, longitude: 0)
+    
+    let annotations: [AnnotationClassType] = [
+        CIAnnotationWithSwiftUI(coordinate: CLLocationCoordinate2D(latitude: 37.58217, longitude: 127.001893), title: "혜하역",uiImage: UIImage(named: Bundle.main.provideFilePath(name: "dummyImage1", ext: "jpeg"))!),
+        
+        CIAnnotationWithSwiftUI(coordinate: CLLocationCoordinate2D(latitude: 37.5805009, longitude: 127.0028245), title: "마로니에 공원", uiImage: UIImage(named: Bundle.main.provideFilePath(name: "dummyImage2", ext: "jpeg"))!)
+    ]
+    
+    var mapView: CJMapkitView {
+        CJMapkitView(userLocation: currentLocation, annotations: annotations)
+    }
+    
+    var body: some View {
+        ZStack {
+            mapView
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.orange)
+                        .padding(3)
+                        .background(
+                            Circle()
+                                .fill(.white)
+                        )
+                        .frame(width: 75, height: 75)
+                        .onTapGesture {
+                            CJLocationManager.shared.manager.requestLocation()
+                        }
+                        .padding([.trailing, .bottom], 10)
+                    
+                }
+            }
         }
     }
 }
